6fc22e62fe0e281878068c9d49fddcb436f08e8b
Gubina Irina <irina.gubina98@mail.ru>
2020-09-20 18:24:54 +0300

    Добавлен src/

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int cost = 10_000;
+        int miles = service.calculate(10_000);
+        System.out.println(miles);
+
+
+    }
+}
