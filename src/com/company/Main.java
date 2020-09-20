e8bece4e2a6922a3a02543bf82f622ca21b799f9
Gubina Irina <irina.gubina98@mail.ru>
2020-09-20 21:08:01 +0300

    Изменения в src/

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int cost = 10_000;
-        int miles = service.calculate(10_000);
+        int miles = service.calculate (cost);
         System.out.println(miles);
 
 
